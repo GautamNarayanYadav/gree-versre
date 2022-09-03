ee20906def555d572cb5ce3b8c6aa1957cffdaa2
gautamyadav <gautamyadav@spliceglobal.com>
2022-09-03 21:07:18 +0530

    add order_by

--- g_vers/admin.py
+++ g_vers/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from g_vers.models import *
 
 
+class PhotographyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'upload_by', 'upload_date', 'upload_date')
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'mobile', 'address', 'message')
+
+
 admin.site.register(Song)
-admin.site.register(Photography)
-admin.site.register(Contact)
+admin.site.register(Photography, PhotographyAdmin)
+admin.site.register(Contact, ContactAdmin)
